c7cd42e0f4ce07cd95af9b95f75119e6b036ce13
stuttgartseo <79091447+stuttgartseo@users.noreply.github.com>
2024-05-27 22:38:12 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://stuttgartseo.github.io/',
+	site: 'http://freelancerstuttgart.de/',
 	integrations: [mdx(), sitemap()],
 });
