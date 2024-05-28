c5be333251076a55e3c4280ef4e2ac0d51209bcf
username <90975164+adamswebsite@users.noreply.github.com>
2024-05-28 11:07:12 +0200

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
 
-import netlify from "@astrojs/netlify";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://preeminent-basbousa-fd0aa7.netlify.app',
-  integrations: [mdx()],
-  output: "server",
-  adapter: netlify()
-});
+  site: 'https://seostuttgart.github.io',
+})
