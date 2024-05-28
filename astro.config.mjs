9a9081f93300f834003b5a46bef222ab9e09b3c0
stuttgartseo <79091447+stuttgartseo@users.noreply.github.com>
2024-05-28 10:28:18 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://freelancerstuttgart.de/',
+	site: 'https://freelancerstuttgart.github.io/',
 	integrations: [mdx()],
 });
